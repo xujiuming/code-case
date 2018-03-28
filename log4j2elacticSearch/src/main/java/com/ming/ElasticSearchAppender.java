c7acf6fcf04d0a8fc33d5da93067fbdefa29d60a
ming <18120580001@163.com>
2018-03-28 17:40:21 +0800

    提交socketappender

--- log4j2elacticSearch/src/main/java/com/ming/ElasticSearchAppender.java
+++ log4j2elacticSearch/src/main/java/com/ming/ElasticSearchAppender.java
@@ -10,9 +10,13 @@ import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 
+import java.io.Serializable;
+
 
 /**
  * log4j2  和es交互的适配器
+ * 不采用 socket appender 原因是 socket appender 功能很齐全 但是 比较麻烦 改起来
+ * 不如直接用es client 实现一个简单的appender
  *
  * @author ming
  * @date 2018-03-28 11:39
@@ -21,50 +25,14 @@ import org.apache.logging.log4j.core.layout.PatternLayout;
 public class ElasticSearchAppender extends AbstractAppender {
 
 
+    public ElasticSearchAppender() {
+        super();
+    }
 
-
-        /*  接收配置文件中的参数 */
+    /*  接收配置文件中的参数 */
         @PluginFactory
-        public static FileAppender createAppender(@PluginAttribute("name") String name,
-                                                  @PluginAttribute("fileName") String fileName,
-                                                  @PluginElement("Filter") final Filter filter,
-                                                  @PluginElement("Layout") Layout<? extends Serializable> layout,
-                                                  @PluginAttribute("ignoreExceptions") boolean ignoreExceptions) {
-            if (name == null) {
-                LOGGER.error("no name defined in conf.");
-                return null;
-            }
-            if (layout == null) {
-                layout = PatternLayout.createDefaultLayout();
-            }
-            // 创建文件
-            if (!createFile(fileName)) {
-                return null;
-            }
-            return new FileAppender(name, filter, layout, ignoreExceptions, fileName);
-        }
-
-        private static boolean createFile(String fileName) {
-            Path filePath = Paths.get(fileName);
-            try {
-                // 每次都重新写文件，不追加
-                if (Files.exists(filePath)) {
-                    Files.delete(filePath);
-                }
-                Files.createFile(filePath);
-            } catch (IOException e) {
-                LOGGER.error("create file exception", e);
-                return false;
-            }
-            return true;
-        }
-
-        private void writerFile(byte[] log) {
-            try {
-                Files.write(Paths.get(fileName), log, StandardOpenOption.APPEND);
-            } catch (IOException e) {
-                LOGGER.error("write file exception", e);
-            }
+        public static FileAppender createAppender(@PluginAttribute("name") String name) boolean ignoreExceptions) {
+            return new ElasticSearchAppender(name, filter, layout, ignoreExceptions, fileName);
         }
 
 
